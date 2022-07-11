fb0c71fcdfa21110524bde6180400e48a8f93482
Sikandar-Sharma001 <sikandarsharma765@gmail.com>
2022-07-11 15:27:29 +0530

    Updates

---
+++ precache-manifest.60b749e8ca88b2660a6dfc0aec64f301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28d47a28aee453241fc5",
+    "url": "/my-app/static/css/main.e6788eb8.chunk.css"
+  },
+  {
+    "revision": "28d47a28aee453241fc5",
+    "url": "/my-app/static/js/main.28d47a28.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/my-app/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "19725f720bcd2a583668",
+    "url": "/my-app/static/js/2.19725f72.chunk.js"
+  },
+  {
+    "revision": "e8b56c0b1af39a8bbb4e",
+    "url": "/my-app/static/js/runtime~main.e8b56c0b.js"
+  },
+  {
+    "revision": "6d584177793c6f48009be464d7589bf9",
+    "url": "/my-app/index.html"
+  }
+];
